ae54d2ad453a1eae49231faf39711e3c82a07c66
RajPShinde <rajprakashshinde07@gmail.com>
2020-11-18 22:03:56 -0500

    Add stub for footstep affordance implementation

---
+++ include/footstepAffordance.hpp
@@ -0,0 +1,41 @@
+#ifndef INCLUDE_FOOTSTEPAFFORDANCE_HPP_
+#define INCLUDE_FOOTSTEPAFFORDANCE_HPP_
+
+
+#include <octomap/octomap.h>
+#include <Eigen/Dense>
+#include <features.hpp>
+#include <map>
+#include <ros/ros.h>
+
+namespace costmap
+{
+
+class FootstepAffordance 
+{
+    public:
+        
+        FootstepAffordance();
+        
+        ~FootstepAffordance();
+
+        void run(octomap::OcTree* octomap, 
+                const Eigen::Vector3d& robotStateXYZ, 
+                const Eigen::Vector3d& robotStateRPY, 
+                std::map<std::pair<double, double>, double> &hMap,
+                std::map<std::pair<double, double>, double> &cMap);
+
+        void calculateCost(octomap::OcTree* octomap, const octomap::OcTreeKey& surfaceCellKey, std::map<std::pair<double, double>, double> &cMap);
+
+        void addToHeightmap(Eigen::Vector3d surfaceCellXYZ, std::map<std::pair<double, double>, double> &hMap);
+
+        void addToCostmap(Eigen::Vector3d surfaceCellXYZ, double &totalCost, std::map<std::pair<double, double>, double> &cMap);
+
+    private:    
+
+
+};
+
+}
+
+#endif  //  INCLUDE_FOOTSTEPAFFORDANCE_HPP_
